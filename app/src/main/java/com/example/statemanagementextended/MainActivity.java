7f86249617c24e53603b1209b6163e0ab91f717d
Illia Zaichenko <10376901u@qed.local>
2024-10-03 15:22:25 +0200

    Add func which increase number++

--- app/src/main/java/com/example/statemanagementextended/MainActivity.java
+++ app/src/main/java/com/example/statemanagementextended/MainActivity.java
@@ -1,4 +1,34 @@
 package com.example.statemanagementextended;
 
-public class MainActivity {
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView counter;
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            number = savedInstanceState.getInt()
+        }
+
+        button = findViewById(R.id.button);
+        counter = findViewById(R.id.counter);
+
+        button.setOnClickListener(v -> {
+            number++;
+            counter.setText(String.valueOf("Licznik: "+number));
+        });
+    }
+    @Override
+    protected void 
 }
